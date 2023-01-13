ac8042831091b98808259ea3f910f3c500f59b6e
BChharaw <brenchhar@gmail.com>
2023-01-12 21:46:24 -0500

    small bug fixes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
   return (
     <div
       name="main"
-      className="bg-[#011627] bg-fixed bg-center bg-no-repeat pt-[80px] w-full h-screen"
+      className="bg-[url('https://cc246282-1098-4f57-bdc7-4b8c66f018e5.id.repl.co/back1.png')] bg-fixed bg-center bg-no-repeat pt-[80px] w-full h-screen"
 
     >
       <div className="pl-[20px] max-w-[1080] mx-auto px-8 flex flex-col justify-center h-full">
